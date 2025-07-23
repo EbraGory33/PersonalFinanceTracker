cdd4cb824720e2b1e4e092f5b6bb211f2a80fa74
EbraGory33 <ebrahimgory3@gmail.com>
2025-07-23 12:00:33 -0400

    New feat(auth): add AuthForm component for sign-in/sign-up pages

--- finance-dashboard/app/(auth)/sign-in/page.tsx
+++ finance-dashboard/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import AuthForm from '@/components/AuthForm'
 
-const login = () => {
+const SignIn = () => {
   return (
-    <div>
-      login
-    </div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </section>
   )
 }
 
-export default login
+export default SignIn
